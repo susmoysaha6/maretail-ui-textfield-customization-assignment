7c64bcddd579fe9a342c35973160364a06c53fa1
susmoysaha6 <susmoysaha6@gmail.com>
2022-12-07 12:51:47 +0600

    Border color changed

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,15 +1,15 @@
 import { TextField } from '@mui/material';
 import React from 'react';
-import styles from './Input.module.css'
+import './Input.css'
 
-console.log(styles);
+
 
 const Input = () => {
     return (
-        <div className={styles.inpContiner}>
-
-            <TextField id="outlined-basic" key="Outlined" label="Outlined" variant="outlined"
-                InputProps={{ className: styles.input }}
+        <div className="inpContiner" >
+            <h1>Basic outlined text field</h1>
+            <TextField id="outlined-basic" label="Outlined" variant="outlined"
+                className='input'
             />
         </div>
     );
